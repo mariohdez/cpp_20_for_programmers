9618280bada24263b627f8d5ffac00b1cde0daaa
Mario Hernandez <mhmario963@gmail.com>
2023-01-08 18:54:01 -0700

    Initial Commit

---
+++ cpp_20_for_programmers/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_20_for_programmers
+//
+//  Created by Mario Hernandez on 1/8/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
