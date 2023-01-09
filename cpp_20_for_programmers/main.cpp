80d48db46e3382629569f897566e525cf79eebee
Mario Hernandez <mhmario963@gmail.com>
2023-01-09 13:57:19 -0700

    introdcue branching, loops and functions.

--- cpp_20_for_programmers/main.cpp
+++ cpp_20_for_programmers/main.cpp
@@ -4,47 +4,60 @@
 
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    default_random_engine engine{};
-    uniform_int_distribution randomDie{1, 6};
-    
-    int frequency1{0};
-    int frequency2{0};
-    int frequency3{0};
-    int frequency4{0};
-    int frequency5{0};
-    int frequency6{0};
+int rollDice();
 
-    for (int roll{1}; roll <= 60000000; ++roll)
+int main(int argc, const char * argv[]) {
+    enum class Status {keepRolling, won, lost};
+
+    int myPoint{0};
+    Status gameStatus{Status::keepRolling};
+    
+    switch (const int sumOfDice{rollDice()}) {
+        case 7:
+        case 11:
+            gameStatus = Status::won;
+            break;
+        case 2:
+        case 3:
+        case 12:
+            gameStatus = Status::lost;
+            break;
+        default:
+            myPoint = sumOfDice;
+            cout << fmt::format("Point is {}\n", myPoint); break;
+    }
+    
+    while (Status::keepRolling == gameStatus)
     {
-        switch (const int face{randomDie(engine)})
-        {
-            case 1:
-                ++frequency1;
-                break;
-            case 2:
-                ++frequency2;
-                break;
-            case 3:
-                ++frequency3;
-                break;
-            case 4:
-                ++frequency4;
-                break;
-            case 5:
-                ++frequency5;
-                break;
-            case 6:
-                ++frequency6;
-                break;
-            default:
-                cout << "Program should never get here!";
-                break;
+        if (const int sumOfDice{rollDice()}; sumOfDice == myPoint) {
+            gameStatus = Status::won;
+        }
+        else if (sumOfDice==7) {
+            gameStatus = Status::lost;
         }
     }
-
-    cout << fmt::format("{:>4}{:>13}\n", "Face", "Frequency");
-    cout << fmt::format("{:>4d}{:>13d}\n", 1, frequency1)  << fmt::format("{:>4d}{:>13d}\n", 2, frequency2) << fmt::format("{:>4d}{:>13d}\n", 3, frequency3)<< fmt::format("{:>4d}{:>13d}\n", 4, frequency4)  << fmt::format("{:>4d}{:>13d}\n", 5, frequency5) << fmt::format("{:>4d}{:>13d}\n", 6, frequency6);
+    
+    if (Status::won == gameStatus)
+    {
+        cout << "Player wins\n";
+    }
+    else
+    {
+        cout << "Player loses\n";
+    }
 
     return 0;
+}
+
+int rollDice() {
+    static random_device rd;
+    static default_random_engine engine{rd()};
+    static uniform_int_distribution randomDie{1, 6};
+    const int die1{randomDie(engine)};
+    const int die2{randomDie(engine)};
+    const int sum{die1 + die2};
+
+    cout << fmt::format("Player rolled {} + {} = {}\n", die1, die2, sum);
+
+    return sum;
 }
