0121625b269d01e494937e1ec3896b6fe76b04c9
Mario Hernandez <mhmario963@gmail.com>
2023-01-09 14:40:38 -0700

    add std::array example

--- cpp_20_for_programmers/main.cpp
+++ cpp_20_for_programmers/main.cpp
@@ -1,63 +1,21 @@
 #include <fmt/format.h>
 #include <iostream>
-#include <random>
-
-using namespace std;
-
-int rollDice();
+#include <array>
 
 int main(int argc, const char * argv[]) {
-    enum class Status {keepRolling, won, lost};
-
-    int myPoint{0};
-    Status gameStatus{Status::keepRolling};
+    std::array<int, 5> values;
     
-    switch (const int sumOfDice{rollDice()}) {
-        case 7:
-        case 11:
-            gameStatus = Status::won;
-            break;
-        case 2:
-        case 3:
-        case 12:
-            gameStatus = Status::lost;
-            break;
-        default:
-            myPoint = sumOfDice;
-            cout << fmt::format("Point is {}\n", myPoint); break;
+    for (size_t i{0}; i < values.size(); ++i)
+    {
+        values[i] = 0;
     }
     
-    while (Status::keepRolling == gameStatus)
-    {
-        if (const int sumOfDice{rollDice()}; sumOfDice == myPoint) {
-            gameStatus = Status::won;
-        }
-        else if (sumOfDice==7) {
-            gameStatus = Status::lost;
-        }
-    }
+    std::cout << fmt::format("{:>7}{:>10}\n", "Element", "Value");
     
-    if (Status::won == gameStatus)
+    for (size_t i{0}; i < values.size(); ++i)
     {
-        cout << "Player wins\n";
-    }
-    else
-    {
-        cout << "Player loses\n";
+        std::cout << fmt::format("{:>7}{:>10}\n", i, values[i]);
     }
 
     return 0;
-}
-
-int rollDice() {
-    static random_device rd;
-    static default_random_engine engine{rd()};
-    static uniform_int_distribution randomDie{1, 6};
-    const int die1{randomDie(engine)};
-    const int die2{randomDie(engine)};
-    const int sum{die1 + die2};
-
-    cout << fmt::format("Player rolled {} + {} = {}\n", die1, die2, sum);
-
-    return sum;
 }
