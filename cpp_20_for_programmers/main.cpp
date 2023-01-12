a7ee4462f5dd16677f0fcec443beefd0d6d7096c
Mario Hernandez <mhmario963@gmail.com>
2023-01-11 19:45:21 -0700

    add range-based for loop exmaple

--- cpp_20_for_programmers/main.cpp
+++ cpp_20_for_programmers/main.cpp
@@ -3,19 +3,26 @@
 #include <array>
 
 int main(int argc, const char * argv[]) {
-    std::array<int, 5> values;
-    
-    for (size_t i{0}; i < values.size(); ++i)
-    {
-        values[i] = 0;
-    }
-    
-    std::cout << fmt::format("{:>7}{:>10}\n", "Element", "Value");
-    
-    for (size_t i{0}; i < values.size(); ++i)
-    {
-        std::cout << fmt::format("{:>7}{:>10}\n", i, values[i]);
-    }
+    std::array items{1, 2, 3, 4, 5};
+
+      // display items before modification
+      std::cout << "items before modification: ";
+      for (const int& item : items) {
+          std::cout << fmt::format("{} ", item);
+      }
+
+      // multiply the elements of items by 2
+      for (int& item : items) {
+         item *= 2;
+      }
+
+      // display items after modification
+      std::cout << "\nitems after modification: ";
+      for (const int& item : items) {
+         std::cout << fmt::format("{} ", item);
+      }
+
+    std::cout << std::endl;
 
     return 0;
 }
