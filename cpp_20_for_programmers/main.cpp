b14911f5197fa57b22c71f9130104a83a27e4839
Mario Hernandez <mhmario963@gmail.com>
2023-01-11 19:53:54 -0700

    add 2-D std::array example.

--- cpp_20_for_programmers/main.cpp
+++ cpp_20_for_programmers/main.cpp
@@ -2,27 +2,25 @@
 #include <iostream>
 #include <array>
 
+constexpr size_t rows{2};
+constexpr size_t columns{3};
+
 int main(int argc, const char * argv[]) {
-    std::array items{1, 2, 3, 4, 5};
-
-      // display items before modification
-      std::cout << "items before modification: ";
-      for (const int& item : items) {
-          std::cout << fmt::format("{} ", item);
-      }
-
-      // multiply the elements of items by 2
-      for (int& item : items) {
-         item *= 2;
-      }
-
-      // display items after modification
-      std::cout << "\nitems after modification: ";
-      for (const int& item : items) {
-         std::cout << fmt::format("{} ", item);
-      }
-
-    std::cout << std::endl;
+    constexpr std::array<std::array<int, columns>, rows> values{
+        {
+            {1,2,3},
+            {4,5,6},
+        }
+    };
+    
+    for (size_t row{0}; row < rows; ++row)
+    {
+        for (size_t column{0}; column < columns; ++column)
+        {
+            std::cout << values.at(row).at(column) << " ";
+        }
+        std::cout << "\n";
+    }
 
     return 0;
 }
